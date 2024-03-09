deacfaf5b15e675fcd2de179ac703c817359071c
Sharma Natvar <krish997854@gmail.com>
2024-03-09 17:13:07 +0530

    normal

---
+++ NodeJs/fsServer.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+
+const a = fs.readFileSync('file.txt' , 'utf-8')
+
+console.log(a);
+
+
+console.log('name');
---
+++ NodeJs/server.js
@@ -0,0 +1,13 @@
+// node warpper funtion
+
+// (function(module , exports, __dirname, require , __filename){
+
+// })
+
+// console.log(module , exports, __dirname, require , __filename);
+
+
+const os = require('os')
+
+console.log(os.freemem());
+console.log(os.homedir());
